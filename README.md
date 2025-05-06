5aed213e07d599b166ebd7b0e011e5636a3f2292
patt. <43396991+Rattanapatt@users.noreply.github.com>
2025-05-06 12:40:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Tic-Tac-Toe
+
+This is a "Tic-Tac-Toe" game. It's a simple project good to start learning ReactJS.
+
+To start the project
+
+```terminal
+npm vite@latest . -- --template react
+```
+
+To run a project locally
+
+```terminal
+npm run dev
+```
+
+This project is a part on React official page to get start with React accessing via the link below https://react.dev/learn/tutorial-tic-tac-toe
+
+React feature using in this project is `useState`. This function is used to manipulate varible.
+
+```js
+(variable,setVariable) = useState(initialValue)
+```
+
+To use in another part of the code is to call `variable`. Initially the value of `variable` is `initialValue`. To assign new value of `newValue` to `variable` is to call `useState` under the parenthesis assign the `newValue` like `useState(newValue)`.
---
+++ src/App.jsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+
+    if (squares[i] || calculateWinner(squares)) {
+      console.log("invalide move");
+      return;
+    }
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares);
+  }
+  const winner = calculateWinner(squares);
+  let gameStatus;
+
+  if (winner) {
+    gameStatus = "Winner: " + winner;
+  } else {
+    gameStatus = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  function calculateWinner(sqaures) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        sqaures[a] &&
+        sqaures[a] === sqaures[b] &&
+        squares[a] === squares[c]
+      ) {
+        return sqaures[a];
+      }
+    }
+    return null;
+  }
+
+  return (
+    <>
+      <div className="gameStatus">{gameStatus}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((sqaures, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
